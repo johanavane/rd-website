89b922a452e372eb9233d60f411cc10da22703b4
Jovmarti <jovmarti@ucsc.edu>
2023-05-04 00:01:57 -0700

    added price to services and centered text

--- rd-hairstylist/src/components/Pricing.js
+++ rd-hairstylist/src/components/Pricing.js
@@ -24,33 +24,65 @@ function Pricing() {
           <div className="box-ht">
             <h3>HAIRCUTS & TREATMENTS</h3>
             <ul>
-              <li>Haircut</li>
-              <li>Fringe Trim</li>
-              <li>Trim Added Onto Color Service</li>
-              <li>Conditioning Treatment</li>
+              <li>
+                Haircuts <span className="right">$75</span>
+              </li>
+              <li>
+                Fringe Trim <span className="right">$15</span>
+              </li>
+              <li>
+                Trim Added Onto Color Service <span className="right">$50</span>
+              </li>
+              <li>
+                Conditioning Treatment <span className="right">$35-$100</span>
+              </li>
               {/* <span>Price depends on treatment choice</span> */}
-              <li>Wash & Style</li>
+              <li>
+                Wash & Style <span className="right">$50</span>
+              </li>
             </ul>
           </div>
           <div className="box-color">
             <h3>COLORING SERVICES</h3>
             <ul>
-              <li>Base Color Root Retouch</li>
-              <li>All Over Color</li>
-              <li>Toner With Blow Dry/Style</li>
-              <li>Base With Toner</li>
+              <li>
+                Base Color Root Retouch <span className="right">$85</span>
+              </li>
+              <li>
+                All Over Color <span className="right">$135+</span>
+              </li>
+              <li>
+                Toner With Blow Dry/Style <span className="right">$85</span>
+              </li>
+              <li>
+                Base With Toner <span className="right">$120</span>
+              </li>
             </ul>
           </div>
           <div className="box-light">
             <h3>LIGHTENING SERVICES</h3>
             <ul>
-              <li>Full Head Balayage</li>
-              <li>Partial Head Balayage</li>
-              <li>Babylights</li>
-              <li>Full Highlight</li>
-              <li>Partial Highlight</li>
-              <li>Tipping Out Ends</li>
-              <li>Face-frame Highlights</li>
+              <li>
+                Full Head Balayage <span className="right">$280</span>
+              </li>
+              <li>
+                Partial Head Balayage <span className="right">$225+</span>
+              </li>
+              <li>
+                Babylights <span className="right">$300+</span>
+              </li>
+              <li>
+                Full Highlight <span className="right">$200+</span>
+              </li>
+              <li>
+                Partial Highlight <span className="right">$165+</span>
+              </li>
+              <li>
+                Tipping Out Ends <span className="right">$25+</span>
+              </li>
+              <li>
+                Face-frame Highlights <span className="right">$50+</span>
+              </li>
             </ul>
           </div>
         </section>
