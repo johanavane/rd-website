0344dba758eaa753a467869e21004a31a634dd0c
Jovmarti <jovmarti@ucsc.edu>
2023-05-17 13:56:40 -0700

    moved button on home compoent to left, changed text size on pricing component

--- rd-hairstylist/src/components/Pricing.js
+++ rd-hairstylist/src/components/Pricing.js
@@ -4,89 +4,103 @@ import "../styles/Pricing.css";
 function Pricing() {
   return (
     <div className="pricing-container" id="pricing">
-      <div className="pricing-flex">
-        <section className="pricing-note">
-          <h2>PRICING</h2>
-          <p>
-            Please note pricing for services may fluctuate based on hair length,
-            density, and amount of product used.
-          </p>
-          <p>
-            All lightening services include root shadowing, toning, blow drying
-            and styling.
-          </p>
-          <p>
-            All color services are base pricing, additional product after 2
-            bowls of color are $10/bowl.
-          </p>
-        </section>
-        <section className="pricing-list">
-          <div className="box-ht">
-            <h3>HAIRCUTS & TREATMENTS</h3>
-            <ul>
-              <li>
-                Haircuts <span className="right">$75</span>
-              </li>
-              <li>
-                Fringe Trim <span className="right">$15</span>
-              </li>
-              <li>
-                Trim Added Onto Color Service <span className="right">$50</span>
-              </li>
-              <li>
+      <section className="pricing-note">
+        <h2>PRICING</h2>
+        <p>
+          Please note pricing for services may fluctuate based on hair length,
+          density, and amount of product used.
+        </p>
+        <p>
+          All lightening services include root shadowing, toning, blow drying
+          and styling.
+        </p>
+        <p>
+          All color services are base pricing, additional product after 2 bowls
+          of color are $10/bowl.
+        </p>
+      </section>
+      <section className="pricing-boxes">
+        <div className="box-haircuts">
+          <h3>HAIRCUTS & TREATMENTS</h3>
+          <ul>
+            <li>
+              Haircuts <span className="right">$75</span>
+            </li>
+            <li>
+              Fringe Trim <span className="right">$15</span>
+            </li>
+            <li>
+              Trim Added Onto Color Service <span className="right">$50</span>
+            </li>
+            <div>
+              <li className="needs-info">
                 Conditioning Treatment <span className="right">$35-$100</span>
               </li>
-              {/* <span>Price depends on treatment choice</span> */}
-              <li>
-                Wash & Style <span className="right">$50</span>
+              <li className="add-info">
+                Price depends on treatment <br /> choice.
               </li>
-            </ul>
-          </div>
-          <div className="box-color">
-            <h3>COLORING SERVICES</h3>
-            <ul>
-              <li>
-                Base Color Root Retouch <span className="right">$85</span>
-              </li>
-              <li>
-                All Over Color <span className="right">$135+</span>
-              </li>
-              <li>
-                Toner With Blow Dry/Style <span className="right">$85</span>
-              </li>
-              <li>
-                Base With Toner <span className="right">$120</span>
-              </li>
-            </ul>
-          </div>
-          <div className="box-light">
-            <h3>LIGHTENING SERVICES</h3>
-            <ul>
-              <li>
+            </div>
+            <li>
+              Wash & Style <span className="right">$50</span>
+            </li>
+          </ul>
+        </div>
+        <div className="box-light">
+          <h3>LIGHTENING SERVICES</h3>
+          <ul>
+            <div>
+              <li className="needs-info">
                 Full Head Balayage <span className="right">$280</span>
               </li>
-              <li>
+              <li className="add-info">Including ends.</li>
+            </div>
+            <div>
+              <li className="needs-info">
                 Partial Head Balayage <span className="right">$225+</span>
               </li>
-              <li>
-                Babylights <span className="right">$300+</span>
-              </li>
-              <li>
-                Full Highlight <span className="right">$200+</span>
-              </li>
-              <li>
-                Partial Highlight <span className="right">$165+</span>
-              </li>
-              <li>
-                Tipping Out Ends <span className="right">$25+</span>
-              </li>
-              <li>
+              <li className="add-info">Root touch up. For existing clients.</li>
+            </div>
+            <li>
+              Babylights <span className="right">$300+</span>
+            </li>
+            <li>
+              Full Highlight <span className="right">$200+</span>
+            </li>
+            <li>
+              Partial Highlight <span className="right">$165+</span>
+            </li>
+            <li>
+              Tipping Out Ends <span className="right">$25+</span>
+            </li>
+            <div>
+              <li className="needs-info">
                 Face-frame Highlights <span className="right">$50+</span>
               </li>
-            </ul>
-          </div>
-        </section>
-      </div>
+              <li className="add-info">Touch up only.</li>
+            </div>
+          </ul>
+        </div>
+        <div className="box-color">
+          <h3>COLORING SERVICES</h3>
+          <ul>
+            <li>
+              Base Color Root Retouch <span className="right">$85</span>
+            </li>
+            <div>
+              <li className="needs-info">
+                All Over Color <span className="right">$135+</span>
+              </li>
+              <li className="add-info">Not a lightening service</li>
+            </div>
+            <li>
+              Toner With Blow Dry/Style <span className="right">$85</span>
+            </li>
+            <li>
+              Base With Toner <span className="right">$120</span>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
