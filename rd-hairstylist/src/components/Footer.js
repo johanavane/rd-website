f1df41866ac49c5e41bebd5d5dfaa7cd171bc0f3
Jovmarti <jovmarti@ucsc.edu>
2023-06-23 23:47:15 -0700

    added alt text to images, cleaned code up

--- rd-hairstylist/src/components/Footer.js
+++ rd-hairstylist/src/components/Footer.js
@@ -4,7 +4,7 @@ import "../styles/Footer.css";
 function Footer() {
   return (
     <div className="footer-background">
-      <div className="footer-container">
+      <section className="footer-container">
         <section className="column">
           <span className="rachel">Rachel Depillo</span>
           <a href="https://www.google.com/maps/place/The+Young+American+Salon+-+Southern+Local/@33.7451847,-117.8319899,17z/data=!4m15!1m8!3m7!1s0x80dcd96264c1e473:0x4c830c54a07283d8!2s720+W+First+St,+Tustin,+CA+92780!3b1!8m2!3d33.7451847!4d-117.8319899!16s%2Fg%2F11c13gmjvq!3m5!1s0x80dcd962631f8aaf:0xf4a31d3609ce54fa!8m2!3d33.7454467!4d-117.8319672!16s%2Fg%2F11d_dg3w29">
@@ -41,12 +41,12 @@ function Footer() {
             Yelp
           </a>
         </section>
-      </div>
-      <div>
+      </section>
+      <section>
         <a className="web-design" href="https://www.johanamartinez.com/">
           Web Design by Johana Martinez.
         </a>
-      </div>
+      </section>
     </div>
   );
 }
--- rd-hairstylist/src/components/Services.js
+++ rd-hairstylist/src/components/Services.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "../styles/Services.css";
-import imageTreatments from "../images/treatments.png";
-import imageColor from "../images/color.png";
-import imageLightening from "../images/lightening.png";
 import imageHairColor from "../images/hair-color.png";
 import imageHairLightening from "../images/hair-lightening.png";
 import imageHairTreatment from "../images/hair-treatment.png";
@@ -13,19 +10,28 @@ function Services() {
       <div className="services-flexbox">
         <section className="color">
           <div className="circle-image">
-            <img src={imageHairColor} />
+            <img
+              src={imageHairColor}
+              alt="black and white drawing of hair being dyed"
+            />
           </div>
           <span>Color Services</span>
         </section>
         <section className="lightening">
           <div className="circle-image">
-            <img src={imageHairLightening} />
+            <img
+              src={imageHairLightening}
+              alt="black and white drawing of woman with long hair"
+            />
           </div>
           <span>Lightening Services</span>
         </section>
         <section className="treatments">
           <div className="circle-image">
-            <img src={imageHairTreatment} />
+            <img
+              src={imageHairTreatment}
+              alt="black and white drawing of a hair product bottle"
+            />
           </div>
           <span>Hair Treatments</span>
         </section>
